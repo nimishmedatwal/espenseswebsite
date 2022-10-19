518b078ee77c8b91c9f52a113e9e22979a8ed5b8
nimishmedatwal <medatwalnimishm@gmail.com>
2022-10-19 23:13:35 +0530

    username validification

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,8 @@
-from .views import RegisterView, LoginView
+from .views import RegisterView, LoginView, UsernameValidateView
 from django.urls import path
-
+from django.views.decorators.csrf import csrf_exempt
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
+    path('validate-username/', csrf_exempt(UsernameValidateView.as_view()), name='validate-username'),
 ]
